0ed4328666382d66a46e927f914d36a5d1df395e
Аделина Винник-Крупчан <adavinnik@gmail.com>
2023-07-07 14:27:27 +0200

    Add Models

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,41 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Bids(models.Model):
+    start_bid = models.IntegerField()
+
+    def __str__(self):
+        return f"Start: {self.start_bid}"
+
+class Listing(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=1024)
+    start_bid = models.ForeignKey(Bids, on_delete=models.CASCADE, related_name="bids")
+
+    FASHION = "FN"
+    TOYS = "TY"
+    ELECTRONICS = "EL"
+    HOME = "HM"
+    SPORT = "SP"
+
+    CATEGORY_CHOICES = [
+        (FASHION, 'Fashion'),
+        (TOYS, 'Toys'),
+        (ELECTRONICS, 'Electronics'),
+        (HOME, 'Home'),
+        (SPORT, 'Sport'),
+    ]
+    category = models.CharField(max_length=2, choices=CATEGORY_CHOICES, default=FASHION)
+    photo = models.URLField()
+
+    def __str__(self):
+        return f"{self.id}: {self.title} - {self.description}"
+
+class Comments(models.Model):
+    listings = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    comment = models.ManyToManyField(Listing, blank=True, related_name="listing")
+
+    def __str__(self):
+        return f"{self.listings}: {self.comment}"
+
